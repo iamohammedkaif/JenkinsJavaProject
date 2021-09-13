67a065b7e2e0d48019f778b13a169bad5a209c74
iamohammedkaif <mohammedkaifs1210@gmail.com>
2021-09-13 10:51:44 +0530

    First commit of java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+class Hello
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello this is java program");
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("Value of i "+i);
+        }
+    }
+    
+}
